1ff5a8d9ce017ecb1c43b9ae3db2884496e0127b
Christian Kniep <ckniep@gaikai.com>
2015-08-10 17:02:34 -0700

    zookafka w/ kafka ontop of zookeeper

--- opt/qnib/kafka/bin/check_kafka.sh
+++ opt/qnib/kafka/bin/check_kafka.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --list|xargs)
+TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper leader.zookeeper.service.consul:2181 --list|xargs)
 EC=$?
 if [ $EC -ne 0 ];then
     echo "/opt/kafka/bin/kafka-topics.sh returns EC:'${EC}'"
     return ${EC}
 fi
 if [ "X${TOPICS}" == "X" ];then
-	/opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --topic syslog --create --partitions 1 --replication-factor 1
+	/opt/kafka/bin/kafka-topics.sh --zookeeper leader.zookeeper.service.consul:2181 --topic syslog --create --partitions 1 --replication-factor 1
 fi
 
-TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --list|xargs)
+TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper leader.zookeeper.service.consul:2181 --list|xargs)
 EC=$?
 if [ $EC -ne 0 ];then
     echo "/opt/kafka/bin/kafka-topics.sh returns EC:'${EC}'"
